94f8564a9978a446e19bfcc03ce2b7a9192c5fb6
chaudhuree <chaudhuree00@gmail.com>
2024-01-15 00:47:01 +0600

    array type

--- note__one/src/main.ts
+++ note__one/src/main.ts
@@ -1,24 +1,7 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+// array
+// let a: number[]; // sometime this may cause error if we don't initialize it
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+let a: number[] = [];
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+a = [1,2,3,4,5,6,7,8,9,10];
+console.log('a', a);
