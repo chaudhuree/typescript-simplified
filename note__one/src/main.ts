dce860106d1193cd23863b8c36f6e2ca360543c4
chaudhuree <chaudhuree00@gmail.com>
2024-01-15 01:06:27 +0600

    object type

--- note__one/src/main.ts
+++ note__one/src/main.ts
@@ -5,3 +5,11 @@ let a: number[] = [];
 
 a = [1,2,3,4,5,6,7,8,9,10];
 console.log('a', a);
+
+// object 
+
+let person:{name:string,age:number,isMarried?:boolean}={
+    name: 'John',
+    age: 30
+}
+person.isMarried = true;
